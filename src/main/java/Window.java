2e50a957912539e922e75c57808248fce8932bc8
Ricardo Rocha <me@rrocha.uk>
2021-06-13 00:29:12 +0100

    .gitignore

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -51,9 +51,7 @@ public class Window extends JFrame {
 
             @Override
             public void keyReleased(KeyEvent keyEvent) {
-//                if(keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
-//                    entity.setxPos(0);
-//                }
+
             }
         });
     }
